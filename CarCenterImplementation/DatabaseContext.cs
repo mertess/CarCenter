42dae3df1cb3bf4b2c7fdf7e41fc4c39ebae7903
mertess <mitya.lagin@yandex.ru>
2020-04-30 14:11:43 +0400

    init basic proj

---
+++ CarCenterImplementation/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarCenterImplementation
+{
+    public class DatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-BIJFUOL;
+                Initial Catalog=DressesShopDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
